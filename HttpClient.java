ade5c70650130ee612d2321c735193e672a93613
tsvetomiraD <95402102+tsvetomiraD@users.noreply.github.com>
2022-12-30 10:29:53 +0200

    Update HttpClient.java

--- HttpClient.java
+++ HttpClient.java
@@ -1,55 +1,80 @@
-import com.sun.net.httpserver.Headers;
-
 import java.io.*;
 import java.net.HttpURLConnection;
+import java.net.InetAddress;
+import java.net.Socket;
 import java.net.URL;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
 
 public class HttpClient {
     public static HttpClient newHttpClient() {
         return new HttpClient();
     }
 
-    public <T> HttpResponse<T> send(HttpRequest request) throws Exception {
-        URL url = request.uri.toURL();
-        HttpURLConnection con = (HttpURLConnection) url.openConnection();
-        sendRequest(request, con);
+    public <T> HttpResponse<T> send(HttpRequest request, HttpResponse.BodyHandler<T> t) throws Exception {
+        Socket socket = new Socket(request.uri.getHost(), 80);
+        sendRequest(request, socket);
 
-        int code = con.getResponseCode();
-        String content = getResponse(con);
-
-        return new HttpResponse<>(content, code);
+        return getResponse(socket);
     }
 
-    private static String getResponse(HttpURLConnection con) throws IOException {
-        StringBuilder content = new StringBuilder();
+    private <T> HttpResponse getResponse(Socket socket) {
+        T body = null;
+        int code = 0;
+        Map<String, String> headers = new HashMap<>();
+        
+        try(BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+            String line = in.readLine();
+            code = Integer.parseInt(line.split(" ")[1]);
+            
+            line = in.readLine();
+            while (line != null) {
+                if (line.equals("")) {
+                    body = (T) in.readLine();
+                    break;
+                }
+                
+                String[] r = line.split(": ");
+                String key = r[0];
+                if (key.equals("access-control-expose-headers")) {
+                    line = in.readLine();
+                    continue;
+                }
+                
+                String value = r[1];
+                headers.put(key, value);
 
-        try (BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()))) {
-            String inputLine;
-            while ((inputLine = in.readLine()) != null) {
-                content.append(inputLine);
+                line = in.readLine();
             }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
 
-        con.disconnect();
-
-        return content.toString();
+        return new HttpResponse<>(body, code, headers);
     }
 
-    private static void sendRequest(HttpRequest request, HttpURLConnection con) throws IOException {
-        for (var s : request.headers.entrySet()) {
-            con.setRequestProperty(s.getKey(), s.getValue());
-        }
-        con.setRequestMethod(request.method);
+    private static <T> void saveToFile(Path path, T body) throws IOException {
+        File f = new File(path.toUri());
 
-        if (request.body == null) {
-            return;
+        try (FileWriter fileWriter = new FileWriter(f)) {
+            //fileWriter.write(body);
+            fileWriter.flush();
         }
+    }
+    
+    private static void sendRequest(HttpRequest request, Socket socket) throws IOException {
+        try (PrintWriter out = new PrintWriter(socket.getOutputStream())) {
+            out.println(request.method + " " + request.uri.getPath() + " HTTP/1.1\r\n");
+            out.println("Host: " + request.uri.getHost());
 
-        con.setDoOutput(true);
-        try (OutputStream outStream = con.getOutputStream();
-             OutputStreamWriter outStreamWriter = new OutputStreamWriter(outStream, "UTF-8")) {
-            outStreamWriter.write(request.body);
-            outStreamWriter.flush();
+            for (var m : request.headers.entrySet()) {
+                out.println(m.getKey() + ": " + m.getValue() + "\r\n");
+            }
+            out.flush();
+
+            out.println(request.body);
+            out.flush();
         }
     }
 }
